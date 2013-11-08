3877a37106edbf1d61607f6ab3c18f85a3c4bf2b
Alex Paz <alexander.j.paz@ngc.com>
2013-11-08 09:51:02 -0500

    removed work specific references

--- .bash.d/alias_and_functions.sh
+++ .bash.d/alias_and_functions.sh
@@ -1,22 +1,10 @@
 # User specific aliases and functions
-alias ncd='cd $NGA_HOME'
-alias cbf='cd $NGA_HOME'
-alias q="fab -c /home/apaz/.scripts/q/.fabricrc"
-alias gr='_gr'
-alias gcd="_gcd"
+alias paz="fab -c /home/apaz/.scripts/paz/.fabricrc"
 alias cpd="cp -t $HOME/Desktop/"
 alias mci='mvn clean install -Dmaven.test.skip=true'
 
 function _svngrep() {
 	grep --exclude-dir=".svn" -r "$@" .
-}
-
-function _gr() {
-	grep -r "$@" .
-}
-
-function _gcd() {
-	cd /build/cbf.git/$@
 }
 
 function ___script() {
