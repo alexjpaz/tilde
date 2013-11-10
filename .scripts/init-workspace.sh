9df21f7c1dfb72474671cb8b42779085f4e45e41
Alexander Paz <alexjpaz@gmail.com>
2013-11-10 13:22:54 -0500

    Update init-workspace.sh

--- .scripts/init-workspace.sh
+++ .scripts/init-workspace.sh
@@ -1,1 +1,9 @@
+#
+cd $HOME
+git init
+git remote add tilde https://github.com/alexjpaz/tilde.git
+git fetch tilde
+git reset --hard master
+
+#
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
