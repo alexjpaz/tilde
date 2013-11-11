b7cafeea620da8ac37eb9d22cbbb70a0110f12bc
Alex Paz <alexander.j.paz@ngc.com>
2013-11-11 09:25:58 -0500

    added puppet

---
+++ .puppet/modules/alexjpaz/manifests/init.pp
@@ -0,0 +1,33 @@
+package { "vim":
+	        ensure => "installed"
+}
+
+package { "curl" :
+	        ensure => "installed"
+}
+
+package { "tmux":
+	        ensure => "installed"
+}
+
+package { "npm":
+	        ensure => "installed"
+}
+
+package { "git":
+	        ensure => "installed"
+}
+
+package { "python-pip" :
+	        ensure => "installed"
+}
+
+package { "fabric" :
+	        ensure => "installed"
+}
+
+package { "python-virtualenv" :
+	        ensure => "installed"
+}
+
+
