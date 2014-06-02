5ea22cf9959a171eaf8fd3f52f781b6f29772436
Alex Paz <alexander.j.paz@ngc.com>
2014-06-02 08:40:14 -0400

    moved .bash.d

---
+++ .scripts/bash.d/abis_enable_proxy.sh
@@ -0,0 +1,9 @@
+export VAGRANT_HTTP_PROXY=http://gvpxsv3001.abis-sses.abis.net:3128
+export VAGRANT_HTTPS_PROXY=http://gvpxsv3001.abis-sses.abis.net:3128
+export VAGRANT_FTP_PROXY=http://gvpxsv3001.abis-sses.abis.net:3128
+export VAGRANT_NO_PROXY=http://gvpxsv3001.abis-sses.abis.net:3128
+export http_proxy=http://gvpxsv3001.abis-sses.abis.net:3128
+export ftp_proxy=http://gvpxsv3001.abis-sses.abis.net:3128
+export all_proxy=socks://gvpxsv3001.abis-sses.abis.net:3128
+export https_proxy=http://gvpxsv3001.abis-sses.abis.net:3128
+export no_proxy=localhost,127.0.0.0/8,abis.net,virtual,abisnpe.local,10.0.33.0/24,192.168.105.51
