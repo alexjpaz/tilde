02856a9d0203510e23d0338de1548b307544358e
Alex Paz <alexander.j.paz@ngc.com>
2014-06-10 11:31:09 -0400

    g: add bookmakr -a

--- .scripts/env.d/global/bash.d/functions/g.sh
+++ .scripts/env.d/global/bash.d/functions/g.sh
@@ -2,14 +2,36 @@
 BOOKMARK_DIR=$HOME/.pazconf/bookmarks
 
 function g() {
+	local a
+
+	local OPTIND # ! important
+	while getopts "a:" opt; do
+		case "${opt}" in
+			a)
+				local a=${OPTARG}
+				;;
+		esac
+	done
+
+	shift $((OPTIND-1))
+
 	BOOKMARK=$1
 
-	if [ -z $BOOKMARK ]; then
-		find $BOOKMARK_DIR -type f -perm -u=r | sed "s@$BOOKMARK_DIR/@@"
-		return;
-	fi
+	if [ -z ${a} ]; then
+		if [ -z $BOOKMARK ]; then
+			find $BOOKMARK_DIR -type f -perm -u=r | sed "s@$BOOKMARK_DIR/@@"
+			return;
+		fi
 
-	pushd $(cat ${BOOKMARK_DIR}/$BOOKMARK)
+		if [ -f ${BOOKMARK_DIR}/$BOOKMARK ]; then
+			pushd $(cat ${BOOKMARK_DIR}/$BOOKMARK)
+		else
+			echo "Bookmark does not exist!"
+		fi
+	else
+		mkdir -p ${BOOKMARK_DIR}/$(dirname ${a})
+		echo $PWD > ${BOOKMARK_DIR}/${a}
+	fi
 }
 
 __g()
