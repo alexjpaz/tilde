989bf808110953611341c00404bae977accbe7a1
Alex Paz <alexander.j.paz@ngc.com>
2014-06-10 15:00:09 -0400

    comments

--- .scripts/init.d/dconf.sh
+++ .scripts/init.d/dconf.sh
@@ -5,18 +5,18 @@
 # dconf
 
 # vagrant@ubuntu-14:~$ dconf watch /
-/org/mate/terminal/profiles/default/font
-  'Monospace 10'
-
-/org/mate/terminal/profiles/default/font
-  'Monospace 8'
-
-/org/mate/terminal/profiles/default/background-color
-  '#000000000000'
-/org/mate/terminal/profiles/default/foreground-color
-  '#0000FFFF0000'
-
-/org/mate/terminal/profiles/default/background-color
-  '#000000000000'
-/org/mate/terminal/profiles/default/foreground-color
-  '#FFFFFFFFFFFF'
+#/org/mate/terminal/profiles/default/font
+#  'Monospace 10'
+#
+#/org/mate/terminal/profiles/default/font
+#  'Monospace 8'
+#
+#/org/mate/terminal/profiles/default/background-color
+#  '#000000000000'
+#/org/mate/terminal/profiles/default/foreground-color
+#  '#0000FFFF0000'
+#
+#/org/mate/terminal/profiles/default/background-color
+#  '#000000000000'
+#/org/mate/terminal/profiles/default/foreground-color
+#  '#FFFFFFFFFFFF'
