3dbf4ced979d41f19d853045b1c056285de78809
Alexander Paz <alexjpaz@gmail.com>
2014-06-10 07:22:06 -0400

    Create dconf.sh

---
+++ .scripts/init.d/dconf.sh
@@ -0,0 +1,22 @@
+# apply dconf/gsettings
+
+# TODO: detect bin for setting
+
+# dconf
+
+# vagrant@ubuntu-14:~$ dconf watch /
+/org/mate/terminal/profiles/default/font
+  'Monospace 10'
+
+/org/mate/terminal/profiles/default/font
+  'Monospace 8'
+
+/org/mate/terminal/profiles/default/background-color
+  '#000000000000'
+/org/mate/terminal/profiles/default/foreground-color
+  '#0000FFFF0000'
+
+/org/mate/terminal/profiles/default/background-color
+  '#000000000000'
+/org/mate/terminal/profiles/default/foreground-color
+  '#FFFFFFFFFFFF'
