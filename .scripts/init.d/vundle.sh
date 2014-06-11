c99cc986ee7349c7b2166bd77884f983727c6117
Alex Paz <alexander.j.paz@ngc.com>
2014-06-11 14:06:16 -0400

    pull latest vundle

--- .scripts/init.d/vundle.sh
+++ .scripts/init.d/vundle.sh
@@ -1,2 +1,6 @@
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+VUNDLE_DIR=$HOME/.vim/bundle/vundle
+git clone https://github.com/gmarik/vundle.git ${VUNDLE_DIR}
+cd ${VUNDLE_DIR}
+git pull
+
 vim +PluginInstall +qall
