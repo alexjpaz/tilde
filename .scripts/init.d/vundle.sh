14e4329a3a0c0e7fcd75c098207317a689fd0a07
Alex Paz <alexander.j.paz@ngc.com>
2014-06-12 08:39:26 -0400

    updated vundle.sh

--- .scripts/init.d/vundle.sh
+++ .scripts/init.d/vundle.sh
@@ -1,6 +1,7 @@
-VUNDLE_DIR=$HOME/.vim/bundle/vundle
-git clone https://github.com/gmarik/vundle.git ${VUNDLE_DIR}
+VUNDLE_DIR=$HOME/.vim/bundle/Vundle.vim
+git clone https://github.com/gmarik/Vundle.vim.git ${VUNDLE_DIR}
 cd ${VUNDLE_DIR}
+git stash
 git pull
 
 vim +PluginInstall +qall
