ba0df404e606b0d9ec0cbe8cd91e75817edd07d2
Alex Paz <alexander.j.paz@ngc.com>
2014-07-02 09:15:06 -0400

    updated links for vim env

--- .scripts/init.d/links.sh
+++ .scripts/init.d/links.sh
@@ -4,3 +4,4 @@ ENV="${HOME}/.scripts/env.d/${PAZENV:-${HOSTNAME}}"
 
 ln -sf "${ENV}/git/gitconfig" "${HOME}/.gitconfig"
 ln -sf "${ENV}" "${HOME}/.scripts/env.d/current"
+ln -sf "${ENV}/vim" "${HOME}/.vim/plugin/env"
