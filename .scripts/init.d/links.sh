b68be95a3083479356d660811845c0ab298f5f72
Alex Paz <alexjpaz@gmail.com>
2014-07-18 21:56:03 -0400

    updated init.d/links

--- .scripts/init.d/links.sh
+++ .scripts/init.d/links.sh
@@ -1,7 +1,21 @@
 echo "=== $0 ==="
-
+ 
 ENV="${HOME}/.scripts/env.d/${PAZENV:-${HOSTNAME}}"
+GLOBALENV="${HOME}/.scripts/env.d/global"
 
-ln -sf "${ENV}/git/gitconfig" "${HOME}/.gitconfig"
-ln -sf "${ENV}" "${HOME}/.scripts/env.d/current"
-ln -sf "${ENV}/vim" "${HOME}/.vim/plugin/env"
+echo "Setting up current link"
+ln -snfv "${ENV}" "${HOME}/.scripts/env.d/current"
+
+if [ -f "${ENV}/git/gitconfig" ]; then
+	ln -snfv "${ENV}/git/gitconfig" "${HOME}/.gitconfig"
+else
+	ln -snfv "${GLOBALENV}/git/gitconfig" "${HOME}/.gitconfig"
+fi
+
+if [ -f "${ENV}/vim" ]; then
+	ln -snfv "${ENV}/vim" "${HOME}/.vim/plugin/env"
+else
+	ln -snfv "${GLOBALENV}/vim" "${HOME}/.vim/plugin/env"
+fi
+
+
