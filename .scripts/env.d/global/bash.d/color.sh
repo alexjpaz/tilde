78b632de651345d16edbcb47683e994f383e91b5
Alex Paz <alexjpaz@gmail.com>
2014-07-23 12:31:55 -0400

    this should fix the tput errors on non interactive shells

--- .scripts/env.d/global/bash.d/color.sh
+++ .scripts/env.d/global/bash.d/color.sh
@@ -1,4 +1,8 @@
 # Custom bash prompt via kirsle.net/wizards/ps1.html
-PS1="\[$(tput setaf 5)\][\u@\h \W]\\$ \[$(tput sgr0)\]"
-export PS1
+if [ -z $PS1 ]; then
+	INTERACTIVE=nope
+else
+	PS1="\[$(tput setaf 5)\][\u@\h \W]\\$ \[$(tput sgr0)\]"
+	export PS1
+fi
 
