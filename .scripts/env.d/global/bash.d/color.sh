177017f355765681ea958bc03b2b290e5c3aad66
Alex Paz <alexjpaz@gmail.com>
2014-08-05 11:20:48 -0400

    prompt fix

--- .scripts/env.d/global/bash.d/color.sh
+++ .scripts/env.d/global/bash.d/color.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 # Custom bash prompt via kirsle.net/wizards/ps1.html
-if [ -z $PS1 ]; then
+if [[ -z $PS1 ]]; then
 	INTERACTIVE=nope
 else
 	PS1="\[$(tput setaf 5)\][\u@\h \W]\\$ \[$(tput sgr0)\]"
