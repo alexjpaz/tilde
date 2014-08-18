72a35ffe86f0c1bd86e081075332d528f569fefa
Alex Paz <alexjpaz@gmail.com>
2014-08-18 15:47:08 -0400

    lol

---
+++ .scripts/env.d/apaz.abis-sses.abis.net/bash.d/abis.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+#function abis() {
+#}
+
+#__abis()
+#{
+  #cur=${COMP_WORDS[COMP_CWORD]}
+  #COMPREPLY=()
+  #use=$(find $BOOKMARK_DIR -type f -perm -u=r | sed "s@$BOOKMARK_DIR/@@");
+  #COMPREPLY=( $( compgen -W "$use" -- $cur ) )
+#}
+#complete -o default -o nospace -F __g  g
+
+# vim:syntax=sh
